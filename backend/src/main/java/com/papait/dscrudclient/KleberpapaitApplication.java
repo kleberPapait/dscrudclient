274ac773bea86bd0e0fb3620a1f032eb73da74b8
kleber Papait <kleberpapait@outlook.com>
2020-10-07 14:13:26 -0300

    Project created

---
+++ backend/src/main/java/com/papait/dscrudclient/KleberpapaitApplication.java
@@ -0,0 +1,13 @@
+package com.papait.dscrudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KleberpapaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KleberpapaitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/papait/dscrudclient/KleberpapaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papait.dscrudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KleberpapaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
