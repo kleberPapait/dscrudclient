541764a21ca4d9bad041826ff02dcdf7b8b91894
kleber Papait <kleberpapait@outlook.com>
2020-10-09 14:35:40 -0300

    Pagination and insert, update ok

--- backend/src/main/java/com/papait/dscrudclient/services/ClientService.java
+++ backend/src/main/java/com/papait/dscrudclient/services/ClientService.java
@@ -2,7 +2,11 @@ package com.papait.dscrudclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,18 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 import com.papait.dscrudclient.dto.ClientDTO;
 import com.papait.dscrudclient.entities.Client;
 import com.papait.dscrudclient.repositories.ClientRepository;
+import com.papait.dscrudclient.services.exceptions.DatabaseException;
 import com.papait.dscrudclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
-    @Autowired
+	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-    public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
-		
-		return list.map( x-> new ClientDTO(x));
+
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -30,6 +35,46 @@ public class ClientService {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insertClient(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+
+	}
+
+	@Transactional
+	public ClientDTO updateClient(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+
+		}
+
+	}
+	public void deleteProduct(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 }
